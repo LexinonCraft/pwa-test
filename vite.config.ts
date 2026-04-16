e879f3dac7791243fc9c706f8c00941fb701f673
LexinonCraft <willi4wuff@gmail.com>
2026-04-16 20:09:27 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      react(),
+      VitePWA({
+        injectRegister: 'auto',
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        manifest: {
+          name: 'pwa-test',
+          short_name: 'pwa-test',
+          description: 'pwa-test',
+          theme_color: '#ffffff',
+          background_color: '#242424',
+          display: 'standalone',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        },
+        devOptions: {
+          enabled: true,
+          type: 'module',
+          navigateFallback: 'index.html',
+        },
+        base: '/'
+    })
+  ],
+})
