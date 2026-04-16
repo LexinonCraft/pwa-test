8d4483ab756879d5e614d11e2b31ef9842b89cef
LexinonCraft <willi4wuff@gmail.com>
2026-04-16 21:31:44 +0200

    Add link to about page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Main page!</div>
+  return <div>Main page! <Link to="/about">Go to about page</Link></div>
 }
